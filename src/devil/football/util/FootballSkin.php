f4b07fff10f33f783623b5551c3e5971c2a9d162
DEVIL <122738174+DEVILxD5919@users.noreply.github.com>
2025-07-30 15:54:34 +0500

    Update FootballSkin.php

--- src/devil/football/util/FootballSkin.php
+++ src/devil/football/util/FootballSkin.php
@@ -4,37 +4,61 @@ declare(strict_types=1);
 
 namespace devil\football\util;
 
-use JsonException;
 use devil\football\Football;
 use pocketmine\entity\Skin;
+use JsonException;
+use function chr;
 use function file_get_contents;
+use function getimagesize;
+use function imagecolorat;
+use function imagecreatefrompng;
+use function imagedestroy;
 
 class FootballSkin {
-    protected static ?Skin $skin = null;
+    private static ?Skin $skin = null;
 
     /**
+     * Returns the football skin, generating it if not already cached.
+     *
      * @throws JsonException
      */
     public static function get(): Skin {
-        if(self::$skin !== null) {
+        if (self::$skin instanceof Skin) {
             return self::$skin;
         }
-        $instance = Football::getInstance();
-        $image = imagecreatefrompng($instance->getDataFolder()."football.png");
-        $bytes = "";
-        $l = (int) @getimagesize($instance->getDataFolder()."football.png")[1];
-        for ($y = 0; $y < $l; $y++) {
-            for ($x = 0; $x < 64; $x++) {
-                $rgba = @imagecolorat($image, $x, $y);
-                $a = ((~(($rgba >> 24))) << 1) & 0xff;
-                $r = ($rgba >> 16) & 0xff;
-                $g = ($rgba >> 8) & 0xff;
-                $b = $rgba & 0xff;
-                $bytes .= chr($r) . chr($g) . chr($b) . chr($a);
+
+        $plugin = Football::getInstance();
+        $dataFolder = $plugin->getDataFolder();
+
+        $imgPath = $dataFolder . "football.png";
+        $jsonPath = $dataFolder . "football.json";
+
+        $img = imagecreatefrompng($imgPath);
+        $skinBytes = "";
+
+        $height = (int)(@getimagesize($imgPath)[1] ?? 0);
+
+        for ($y = 0; $y < $height; ++$y) {
+            for ($x = 0; $x < 64; ++$x) {
+                $pixel = @imagecolorat($img, $x, $y);
+                $a = ((~($pixel >> 24)) << 1) & 0xFF;
+                $r = ($pixel >> 16) & 0xFF;
+                $g = ($pixel >> 8) & 0xFF;
+                $b = $pixel & 0xFF;
+                $skinBytes .= chr($r) . chr($g) . chr($b) . chr($a);
             }
         }
-        @imagedestroy($image);
-        self::$skin = new Skin("Football", $bytes, "", "geometry.football", file_get_contents($instance->getDataFolder()."football.json"));
+
+        @imagedestroy($img);
+
+        self::$skin = new Skin(
+            "Football",
+            $skinBytes,
+            "",
+            "geometry.football",
+            file_get_contents($jsonPath)
+        );
+
         return self::get();
     }
 }
