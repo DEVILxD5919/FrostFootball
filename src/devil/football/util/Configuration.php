ad1d74c00bacc8e40e1777209df3f8b750bfabdf
DEVIL <122738174+DEVILxD5919@users.noreply.github.com>
2025-07-30 15:53:14 +0500

    Update Configuration.php

--- src/devil/football/util/Configuration.php
+++ src/devil/football/util/Configuration.php
@@ -5,13 +5,15 @@ declare(strict_types=1);
 namespace devil\football\util;
 
 interface Configuration {
-    public const MIN_VERTICAL_ENERGY = 0.09;
-    public const MIN_HORIZONTAL_ENERGY = 0.125;
+    // Energy thresholds
+    public const MIN_VERTICAL_ENERGY    = 0.09;
+    public const MIN_HORIZONTAL_ENERGY  = 0.125;
 
-    public const DEFAULT_SPEED_LOSS = 0.025;
+    // Speed handling
+    public const DEFAULT_SPEED_LOSS     = 0.025;
+    public const IN_AIR_SPEED_INCREASE  = 0.02;
 
-    public const IN_AIR_SPEED_INCREASE = 0.02;
-
-    public const IMPACT_ENERGY_LOSS = 0.1025;
-    public const IMPACT_MOTION_LOSS = 0.9;
+    // Impact adjustments
+    public const IMPACT_ENERGY_LOSS     = 0.1025;
+    public const IMPACT_MOTION_REDUCE   = 0.9;
 }
